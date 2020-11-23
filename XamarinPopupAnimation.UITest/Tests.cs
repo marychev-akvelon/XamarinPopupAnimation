1360cae8b510a66cac6da0c956446141c3e162eb
Mikhail Marychev <v-mimary@microsoft.com>
2020-11-23 18:07:54 +0300

    Start. One test with Thread.Sleep*3

---
+++ XamarinPopupAnimation.UITest/Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+
+namespace XamarinPopupAnimation.UITest
+{
+    [TestFixture(Platform.Android)]
+    // [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            
+            app.WaitForElement(c => c.Marked("Hi"));
+            Thread.Sleep(3000);
+            app.Tap(c => c.Marked("Hi"));
+            app.WaitForElement(c => c.Marked("One"));
+
+            Thread.Sleep(3000);
+            app.Tap(c => c.Marked("One"));
+        }
+    }
+}
---
+++ XamarinPopupAnimation/XamarinPopupAnimation/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinPopupAnimation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HableClick(object o, EventArgs e)
+        {
+            if (!this.popuplayout.IsVisible)
+            {
+                this.popuplayout.IsVisible = !this.popuplayout.IsVisible;
+                this.popuplayout.AnchorX = 1;
+                this.popuplayout.AnchorY = 1;
+
+                Animation scaleAnimation = new Animation(
+                    f => this.popuplayout.Scale = f,
+                    0.5,
+                    1,
+                    Easing.SinInOut);
+
+                Animation fadeAnimation = new Animation(
+                    f => this.popuplayout.Opacity = f,
+                    0.2,
+                    1,
+                    Easing.SinInOut);
+
+                scaleAnimation.Commit(this.popuplayout, "popupScaleAnimation", 250);
+                fadeAnimation.Commit(this.popuplayout, "popupFadeAnimation", 250);
+            }
+            else
+            {
+                // await Task.WhenAny<bool>(this.popuplayout.FadeTo(0, 200, Easing.SinInOut));
+                Task.WhenAny<bool>(this.popuplayout.FadeTo(0, 200, Easing.SinInOut));
+                this.popuplayout.IsVisible = !this.popuplayout.IsVisible;
+            }
+        }
+    }
+}
